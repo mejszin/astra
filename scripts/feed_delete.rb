b8af2884e7b32647194ccfee8c32fad4b0a1c8ec
Louis Machin <ldhmachin@gmail.com>
2022-11-25 14:51:43 +0000

    Added test scripts

--- scripts/feed_delete.rb
+++ scripts/feed_delete.rb
@@ -1,8 +1,10 @@
 require_relative './api.rb'
 
 TASK_ID = 'SkAQWEWb'
+FEED_ID = 'LM6pyps7'
 
-puts get('/tasks/feed', {
+puts get('/tasks/feed/delete', {
     :token => API_TOKEN,
     :task_id => TASK_ID,
+    :feed_id => FEED_ID,
 })
