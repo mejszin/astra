fc8b28a1b122850c1cda83216d667b6477f60846
Louis Machin <ldhmachin@gmail.com>
2022-11-25 14:36:29 +0000

    Added API routes

--- scripts/user_new.rb
+++ scripts/user_new.rb
@@ -1,6 +1,6 @@
 require_relative './api.rb'
 
-USERNAME = ''
-PASSWORD = ''
+USERNAME = ARGV[0]
+PASSWORD = ARGV[1]
 
 puts get('/user/new', { :username => USERNAME, :password => PASSWORD })
--- server/api/lib/tasks.js
+++ server/api/lib/tasks.js
@@ -23,9 +23,38 @@ module.exports = function (app) {
                 description: description,
             },
             variables: {},
-            history: {},
+            feed: {},
         }
         return true;
+    }
+
+    app.locals.getTask = (task_id) => {
+        if (task_id in app.locals.task_data) {
+            return app.locals.task_data[task_id];
+        } else {
+            return undefined;
+        }
+    };
+
+    app.locals.deleteTasks = (task_id) => {
+        if (task_id in app.locals.task_data) {
+            delete app.locals.task_data[task_id];
+        }
+    }
+
+    app.locals.newFeed = (task_id, data) => {
+        if (task_id in app.locals.task_data) {
+            let feed_id = app.locals.randomString();
+            app.locals.task_data.feed[feed_id] = data;
+        }
+    }
+
+    app.locals.deleteFeed = (task_id, feed_id) => {
+        if (task_id in app.locals.task_data) {
+            if (feed_id in app.locals.task_data[task_id].feed) {
+                delete app.locals.task_data[task_id].feed[feed_id];
+            }
+        }
     }
 
     app.get('/tasks', (req, res) => {
@@ -48,6 +77,68 @@ module.exports = function (app) {
         if (app.locals.isToken(token)) {
             let user = app.locals.getUser(token);
             app.locals.newTask(user.id, title, description);
+            app.locals.writeTasks();
+            // Success
+            res.status(200).send();
+        } else {
+            // Unauthorized
+            res.status(401).send();
+        }
+    });
+
+    app.get('/tasks/delete', (req, res) => {
+        console.log('/tasks/delete', req.query);
+        const { token, task_id } = req.query;
+        if (app.locals.isToken(token)) {
+            app.locals.deleteTask(task_id);
+            app.locals.writeTasks();
+            // Success
+            res.status(200).send();
+        } else {
+            // Unauthorized
+            res.status(401).send();
+        }
+    });
+
+    app.get('/tasks/feed', (req, res) => {
+        console.log('/tasks/feed', req.query);
+        const { token, task_id } = req.query;
+        if (app.locals.isToken(token)) {
+            let task = app.locals.getTask(task_id);
+            if (task != undefined) {
+                // Success
+                res.status(200).send(task.feed);
+            } else {
+                // No content
+                res.status(204).send();
+            }
+        } else {
+            // Unauthorized
+            res.status(401).send();
+        }
+    });
+
+    
+    app.post('/tasks/feed/new', (req, res) => {
+        console.log('/tasks/feed/new', req.query);
+        const { token, task_id } = req.query;
+        const data = req.body;
+        if (app.locals.isToken(token)) {
+            app.locals.newFeed(task_id, data);
+            app.locals.writeTasks();
+            // Success
+            res.status(200).send();
+        } else {
+            // Unauthorized
+            res.status(401).send();
+        }
+    });
+
+    app.get('/tasks/feed/delete', (req, res) => {
+        console.log('/tasks/feed/delete', req.query);
+        const { token, task_id, feed_id } = req.query;
+        if (app.locals.isToken(token)) {
+            app.locals.deleteFeed(task_id, feed_id);
             app.locals.writeTasks();
             // Success
             res.status(200).send();
